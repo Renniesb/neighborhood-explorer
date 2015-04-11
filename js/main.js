97d73af44e969390b0ff6f854807e65dcf875726
Rennie Bevineau <rsbevineau@gmail.com>
2015-04-11 10:12:09 -0600

    add markers

--- js/main.js
+++ js/main.js
@@ -1,21 +1,56 @@
-var map, broomfield;
+var map, marker, broomfield;
+
+ var locations = [{name: "Azitra Restaurant", lat: 39.934668, long: -105.135171},{name: "Heaven Dragon Restaurant",lat: 39.939406, long: -105.089853},{name: "Corona's Mexican Grill", lat: 39.946250, long: -105.012949}];
 
  var ExplorerMap = function(){
 
  	broomfield= new google.maps.LatLng(39.925899, -105.132387);
 	
 	var mapOptions = {
-	zoom: 13,
+	zoom: 12,
     center: broomfield
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+
+ 	
+
+
+ 	
+ 	var infowindow = new google.maps.InfoWindow(),i;
+  
+
+ 	for (i = 0; i < locations.length; i++) {
+ 		marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i].lat, locations[i].long),
+            map: map
+        }); 
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i)  {
+            return function() {
+                infowindow.setContent(marker);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+
+     
+    
+
+ 	};
+
 }
 
 var ExplorerViewModel = function(){
 	var self = this;
+	
+  self.coordinates= ko.observableArray(locations);
+
+
 
 	self.term = ko.observable("");
+	
+
+
 }
 
 
