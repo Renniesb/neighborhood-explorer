85971bd777b68172eaa36eaeb0df95dafe930573
Rennie Bevineau <rsbevineau@gmail.com>
2015-05-04 22:34:01 -0600

    improve code quality and responsiveness

--- js/main.js
+++ js/main.js
@@ -4,10 +4,7 @@ var markers = [];
   // create locations objects in an array to be used in marker functions.
  locations = [{name: "Azitra Restaurant", lat: 39.934668, long: -105.135171, markerNum: 0},{name: "Heaven Dragon Restaurant",lat: 39.939406, long: -105.089853, markerNum: 1},{name: "Corona's Mexican Grill", lat: 39.946250, long: -105.012949, markerNum: 2}, {name: "Zaika Indian Cuisine", lat: 39.920982, long:-105.087010, markerNum: 3}];
 
-
-
  var ExplorerMap = function(){
-
 
   broomfield= new google.maps.LatLng(39.925899, -105.132387);
   
@@ -18,6 +15,12 @@ var markers = [];
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
+    google.maps.event.addDomListener(window, "resize", function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(center); 
+});
+
   infowindow = new google.maps.InfoWindow();
   var i;
   // create marker functions to place markers on map and set up the info window
@@ -26,9 +29,7 @@ var markers = [];
             position: new google.maps.LatLng(locations[i].lat, locations[i].long),
             map: map,
             title: locations[i].name
-        }); 
-
-    
+        });    
 
         google.maps.event.addListener(marker, 'click', (function(marker)  {
             return function() {
@@ -36,26 +37,19 @@ var markers = [];
                 // set info window with a title and open the info window
                 //infowindow.setTitle(marker.title);
                 infowindow.setContent(marker.title+"<div id='content'></div>");
-                infowindow.open(map, marker);
-
-                
-
+                infowindow.open(map, marker);  
 
               // add marker animation by setting and timing out animation
                 marker.setAnimation(google.maps.Animation.BOUNCE);
                 setTimeout(function(){ marker.setAnimation(null); }, 750);
                 getApi(marker);
                             
-            }
+            };
         })(marker));
 
         markers.push(marker);
-
-  };
-
-
-
-}
+  }
+};
 
 var ExplorerViewModel = function(){
   var self = this;
@@ -82,30 +76,25 @@ var ExplorerViewModel = function(){
 
      getApi(point); 
 
- }
+ };
  //Handles the showing and hiding of all markers depending on setMap() value
   self.showOrHideMarkers= function(state){
            for (var i = 0; i < markers.length; i++) {
             markers[i].setMap(state);
-          };
-        }
-
+          }
+        };
 // returns array to the filteredmarkers array definition
   self.filterArray = function(filter){
        return ko.utils.arrayFilter(self.locations(), function(location) {
-        return location.name.toLowerCase().indexOf(filter) >= 0;   
-
-                                 
+        return location.name.toLowerCase().indexOf(filter) >= 0;                                    
        });
-
-  }
+  };
 //displays selected markers
   self.displaySelected = function(filteredmarkers){
   for (var i = 0; i < filteredmarkers.length; i++) {
              markers[filteredmarkers[i].markerNum].setMap(map);
             }
-      }
-
+      };
 
 //Manages filtering of list view and markers
 self.filterList = function(){
@@ -122,16 +111,11 @@ var filter = self.filter().toLowerCase();
   return filteredmarkers;
 
   }
-}
+};
 
-
-}
-
-
-
+};
 
 var getApi = function( marker){
-
         var lat= marker.position.lat();
         var long = marker.position.lng();
 
@@ -143,36 +127,21 @@ var getApi = function( marker){
             '4WZUYZGWR&v=20130815' + '&ll=' + lat + ',' +
            long + '&query=\'' + marker.title + '\'&limit=1';
 
-      
-
-
-  $.getJSON(url, function(response){
-
-         
+  $.getJSON(url, function(response){        
 
          var venue = response.response.venues[0];
          var venueLoc = venue.contact.formattedPhone;
          var venueAddress = venue.location.formattedAddress;
-           var venuePhotoPrefix = venue.categories[0].icon.prefix+'bg'+'_64';
+          var venuePhotoPrefix = venue.categories[0].icon.prefix+'bg'+'_64';
            var venuePhoto = venuePhotoPrefix + venue.categories[0].icon.suffix;
-
-   
-        
+           
         $windowContent.append('<p>'+venueLoc+'</p>');
         $windowContent.append('<p>'+venueAddress+'</p>');
-        $windowContent.append('<img src="'+ venuePhoto+'">');
-   
-
+        $windowContent.append('<img src="'+ venuePhoto+'">');   
   }).error(function(e){
     $windowContent.text('Content could not be loaded');
 
   });
-
-}
-
-
-
-
+};
 google.maps.event.addDomListener(window, 'load', ExplorerMap);
-
 ko.applyBindings(new ExplorerViewModel());
